0498657f56fe8443308f705496e804f7e805b79f
Hanno Sternberg <hanno@almostintelligent.de>
2015-12-23 13:43:14 +0100

    Day 23 - Task and Input

--- inputs.py
+++ inputs.py
@@ -3717,4 +3717,50 @@ Damage: 8
 Armor: 2""",
     """Hit Points: 51
 Damage: 9""",
+    """jio a, +16
+inc a
+inc a
+tpl a
+tpl a
+tpl a
+inc a
+inc a
+tpl a
+inc a
+inc a
+tpl a
+tpl a
+tpl a
+inc a
+jmp +23
+tpl a
+inc a
+inc a
+tpl a
+inc a
+inc a
+tpl a
+tpl a
+inc a
+inc a
+tpl a
+inc a
+tpl a
+inc a
+tpl a
+inc a
+inc a
+tpl a
+inc a
+tpl a
+tpl a
+inc a
+jio a, +8
+inc b
+jie a, +4
+tpl a
+inc a
+jmp +2
+hlf a
+jmp -7""",
 ]
